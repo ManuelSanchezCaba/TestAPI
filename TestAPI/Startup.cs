ffbf3840fe8c4be163792cf16a4d35428ba7c6d9
ManuelSanchezCaba <msanchez@simetricaconsulting.com>
2022-09-23 17:53:37 -0400

    a

--- TestAPI/Startup.cs
+++ TestAPI/Startup.cs
@@ -83,7 +83,7 @@ namespace TestAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1"));
             //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
